3b58b1643369f517dc00c0f4ad30eead2837e4c2
Kintz53 <kenny.bintz@outlook.fr>
2024-12-05 14:15:40 +0100

    push 4

--- client.py
+++ client.py
@@ -3,36 +3,41 @@ import json
 
 HOST = "localhost"
 PORT = 9999
-
-
-def send_player_data(file_path):
+def connexion(pseudo,type_de_joueur):
     try:
-        # Charger le fichier JSON
-        with open(file_path, "r") as file:
-            player_data = json.load(file)
-
-        # Préparer la connexion et envoyer les données
         conn = http.client.HTTPConnection(HOST, PORT)
-        headers = {"Content-Type": "application/json"}
-        body = json.dumps(player_data)
+        headers = {"Content-Type": "str"}
+        joueur = {'pseudo': pseudo,'type':type_de_joueur}
+        body=json.dumps(joueur)
         conn.request("POST", "/player_data", body=body, headers=headers)
-
-        # Recevoir la réponse
         response = conn.getresponse()
         if response.status == 200:
             data = response.read().decode()
-            return json.loads(data)  # On suppose une réponse JSON
+            return json.loads(data)
         else:
             return f"Erreur : {response.status} {response.reason}"
     except Exception as e:
         return f"Erreur lors de l'envoi des données : {e}"
-    finally:
-        conn.close()
 
+def deplacement(pseudo,type_de_joueur):
+    try:
+        conn = http.client.HTTPConnection(HOST, PORT)
+        headers = {"Content-Type": "str"}
+        deplac=input('Donner votre direction : droite,gauche,haut ou bas')
+        joueur = {'pseudo': pseudo, 'type': type_de_joueur, 'deplacement':deplac}
+        body = json.dumps(joueur)
+        conn.request("POST", "/player_data", body=body, headers=headers)
+        response = conn.getresponse()
+        if response.status == 200:
+            data = response.read().decode()
+            return json.loads(data)
+        else:
+            return f"Erreur : {response.status} {response.reason}"
+    except Exception as e:
+        return f"Erreur lors de l'envoi des données : {e}"
 
-# Programme principal
 if __name__ == "__main__":
-    file_path = "player.json"  # Chemin vers le fichier JSON du joueur
-    print("Envoi des données du joueur...")
-    response = send_player_data(file_path)
-    print("Réponse du serveur :", response)
+    pseudo = input('pseudo')
+    type_de_joueur = input('Type de joueur : "Loup" ou "Vif" ')
+    connexion(pseudo,type_de_joueur)
+
