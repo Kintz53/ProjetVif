eafd6f543c6dbfd052c626c517080447e817a882
Kintz53 <kenny.bintz@outlook.fr>
2024-12-05 15:48:30 +0100

    push 6

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ HOST = "localhost"
 PORT = 9999
 
 def connexion(pseudo, type_de_joueur):
-    """Envoie une requête pour connecter le joueur."""
     try:
         conn = http.client.HTTPConnection(HOST, PORT)
         headers = {"Content-Type": "application/json"}
@@ -21,13 +20,26 @@ def connexion(pseudo, type_de_joueur):
         return {"error": f"Erreur lors de la connexion : {e}"}
 
 def deplacement(pseudo, type_de_joueur, connect):
-    """Envoie une requête pour déplacer le joueur."""
     try:
         print("Environnement :", connect.get("environment", {}))
         conn = http.client.HTTPConnection(HOST, PORT)
         headers = {"Content-Type": "application/json"}
         deplac = input("Donner votre direction : droite, gauche, haut ou bas : ").lower()
         joueur = {'pseudo': pseudo, 'type': type_de_joueur, 'deplacement': deplac}
+        body = json.dumps(joueur)
+        conn.request("POST", "/player_data", body=body, headers=headers)
+        response = conn.getresponse()
+        if response.status == 200:
+            return json.loads(response.read().decode())
+        else:
+            return {"error": f"Erreur : {response.status} {response.reason}"}
+    except Exception as e:
+        return {"error": f"Erreur lors du déplacement : {e}"}
+def ValidationTour(pseudo,type_de_joueur):
+    try:
+        conn = http.client.HTTPConnection(HOST, PORT)
+        headers = {"Content-Type": "application/json"}
+        joueur = {'pseudo': pseudo, 'type': type_de_joueur}
         body = json.dumps(joueur)
         conn.request("POST", "/player_data", body=body, headers=headers)
         response = conn.getresponse()
@@ -56,6 +68,12 @@ if __name__ == "__main__":
             break
         else:
             print(connect["message"], "\nPosition :", connect["position"])
+
+        continuer=ValidationTour(pseudo,type_de_joueur)
+        while(continuer['status']!='Action'):
+            continuer = ValidationTour(pseudo, type_de_joueur)
+
+
 
     if connect["status"] == "Win":
         print("Félicitations, vous avez gagné !")
